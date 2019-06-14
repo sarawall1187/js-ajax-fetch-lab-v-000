b1a85bca3d83bc3b24b8c1cb0fed0e870392e966
Sara Walker <sarawall1187@gmail.com>
2019-06-14 19:57:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const baseURL = 'https://api.github.com';
+const website = 'https://github.com''
 const user = 'sarawall1187';
 const token = 'da0045433ae6aaa22048f1cb0167c10a90cc1891';
 
@@ -11,7 +12,13 @@ function getToken() {
 
 function forkRepo() {
   const repo = 'learn-co-curriculum/js-ajax-fetch-lab';
-  fetch()
+  fetch(repo, {
+  headers: {
+    Authorization: `token ${token}`
+  }
+})
+  .then(res => res.json())
+  .then(json => console.log(json));
   //use fetch to fork it!
 }
 
