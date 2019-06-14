c850dc5e6af36fca37a3632575bcbda1b01d63d2
Sara Walker <sarawall1187@gmail.com>
2019-06-14 20:20:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const user = 'sarawall1187';
 
 function getToken() {
   return 'da0045433ae6aaa22048f1cb0167c10a90cc1891';
-  
+
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
   // return '';
